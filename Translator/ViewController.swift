860b150f7270253a9c49a1b70e19d56281471bfd
Sai Navaneet <sainavaneet76@gmail.com>
2026-01-20 02:54:20 +0900

    Initial Commit

---
+++ Translator/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Translator
+//
+//  Created by Sai Navaneet on 20/01/26.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
